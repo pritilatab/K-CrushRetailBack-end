234f814b3c5560ec7e8d9e0e605af3696ac09716
pritilatab <59198221+pritilatab@users.noreply.github.com>
2019-12-29 17:31:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -174,7 +174,7 @@ app.get('/prodlist', function(req, res) {
   })
 
   /* API action: Search - Watson STT token authorization */
- /* .get('/speech-to-text/token', function(req, res) {
+  /* .get('/speech-to-text/token', function(req, res) {
 
       console.log(config);
       var methodName = 'stt_token';
